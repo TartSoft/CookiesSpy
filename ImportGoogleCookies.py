cf5e9e2e41bfa5e027734d5b9c2d1aa43f23e7ad
Le Trieu Duong <duong032589@stu.vinschool.edu.vn>
2022-01-09 15:40:42 +0700

    Update ImportGoogleCookies.py

--- ImportGoogleCookies.py
+++ ImportGoogleCookies.py
@@ -6,3 +6,48 @@ import shutil
 from datetime import datetime, timedelta
 import win32crypt 
 from Crypto.Cipher import AES 
+def main():
+    # local sqlite Chrome cookie database path
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                            "Google", "Chrome", "User Data", "default", "Cookies")
+    # copy the file to current directory
+    # as the database will be locked if chrome is currently open
+    filename = "Cookies.db"
+    if not os.path.isfile(filename):
+        # copy file when does not exist in the current directory
+        shutil.copyfile(db_path, filename)
+ # connect to the database
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    # get the cookies from `cookies` table
+    cursor.execute("""
+    SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
+    FROM cookies""")
+key = get_encryption_key()
+    for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
+        if not value:
+            decrypted_value = decrypt_data(encrypted_value, key)
+        else:
+            # already decrypted
+            decrypted_value = value
+        print(f"""
+        Host: {host_key}
+        Cookie name: {name}
+        Cookie value (decrypted): {decrypted_value}
+        Creation datetime (UTC): {get_chrome_datetime(creation_utc)}
+        Last access datetime (UTC): {get_chrome_datetime(last_access_utc)}
+        Expires datetime (UTC): {get_chrome_datetime(expires_utc)}
+        ===============================================================
+        """)
+        # update the cookies table with the decrypted value
+        # and make session cookie persistent
+        cursor.execute("""
+        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
+        WHERE host_key = ?
+        AND name = ?""", (decrypted_value, host_key, name))
+    # commit changes
+    db.commit()
+    # close connection
+    db.close()
+if __name__ == "__main__":
+    main()
